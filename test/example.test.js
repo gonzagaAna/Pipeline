5cd2c1d880ee649b7dfa43631883a25bad09475c
aluno <etecae023@hotmail.com>
2025-06-09 07:38:46 -0300

    Primeiro commit

---
+++ test/example.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Exemplo de Teste', function(){
+
+    it('Deve somar dois números corretamente', function(){
+        assert.strictEqual(1 + 1, 2);
+
+    });
+});
